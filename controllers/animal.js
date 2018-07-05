01b0fd6d395fa960bc3d9408fc58f29ae1d1d0f6
Maynor <mynorlarios.ec10@gmail.com>
2018-07-04 20:03:00 -0600

    create animals api

---
+++ controllers/animal.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var Animal = require ('../models/animal');
+
+function getAnimals(req, res){
+
+    res.status(200).send({
+        message: 'Probando el controlador de animales'
+    })
+}
+
+
+module.exports = {
+    getAnimals
+}
---
+++ routes/animal.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var express = require ('express');
+
+var AnimalController = require ('../controllers/animal');
+
+var api = express.Router();
+
+api.get('/animals', AnimalController.getAnimals);
+//api.get('/animals/:filter');
+api.get('/animals/:id');
+api.post('/animal');
+api.post('/animals');
+api.put('/animal/:id');
+api.delete('/animal/:id');
+api.post('/animal-upload-image/:id');
+
+module.exports = api;
